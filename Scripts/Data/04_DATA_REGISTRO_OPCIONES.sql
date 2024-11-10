0e83058cd0f5bf6f4038822776f868dc3608a35c
jperalta <jperalta@JPERALTALP01>
2024-11-10 16:17:28 -0500

    se agrega inluir bandjea servicio tecnico

--- Scripts/Data/04_DATA_REGISTRO_OPCIONES.sql
+++ Scripts/Data/04_DATA_REGISTRO_OPCIONES.sql
@@ -1,9 +1,22 @@
 USE [DB_AHSECO]
 GO
 
-DECLARE @ID INT
+DECLARE @ID INT,@ID_ANT INT
 
 SELECT @ID=ID FROM TBM_SEGURIDAD_OPCION WHERE NOMBRE='Mantenimientos';
 
-INSERT INTO TBM_SEGURIDAD_OPCION VALUES ('M',NULL,'Servicios','Servicios','~BandejaServicios',2,90,@ID,'fa fa-briefcase',1,'system',GETDATE(),'192.168.1.6',NULL,NULL,NULL)
+SELECT @ID_ANT=ID FROM TBM_SEGURIDAD_OPCION WHERE NOMBRE='Prestación Accesoria';
+
+--INSERT INTO TBM_SEGURIDAD_OPCION VALUES ('M',NULL,'Servicios','Servicios','~BandejaServicios',2,90,@ID,'fa fa-briefcase',1,'system',GETDATE(),'192.168.1.6',NULL,NULL,NULL)
+
+UPDATE TBM_SEGURIDAD_OPCION
+SET Nombre='Servicios',
+	   DESCRIPCION='Servicios',
+	   [URL]='~BandejaServicios',
+	   Nivel=2,
+	   Orden=90,
+	   PADRE=@ID,
+	   ICONO='fa fa-briefcase',
+	   HABILITADO=1
+	   WHERE ID=@ID_ANT;
 
