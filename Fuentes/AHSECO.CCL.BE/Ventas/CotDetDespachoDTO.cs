920e07d74e72d53886174659248ae03c996eee9b
jperalta <jperalta@JPERALTALP01>
2024-11-11 07:51:39 -0500

    se corrige dto

--- Fuentes/AHSECO.CCL.BE/Ventas/CotDetDespachoDTO.cs
+++ Fuentes/AHSECO.CCL.BE/Ventas/CotDetDespachoDTO.cs
@@ -28,6 +28,5 @@ namespace AHSECO.CCL.BE.Ventas
         public string NumFianza { get; set; }
         public decimal? MontoPrestPrin { get; set; }
         public decimal? MontoPrestAcc { get; set; }
-        public string NumFianza { get; set; }
     }
 }
