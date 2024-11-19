22b610d89aa493878881023876d5ed69ca17d575
dbazalar <dbazalar@qualitygardian.com>
2024-11-19 10:32:53 -0500

    Se añade FiltroGarantiasDTO

---
+++ Fuentes/AHSECO.CCL.BE/FiltroGarantiasDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHSECO.CCL.BE
+{
+    public class FiltroGarantiasDTO
+    {
+        public List<ComboDTO> Empresas { get; set; }
+        public List<ComboDTO> Estados { get; set; }
+        public List<ComboDTO> Clientes { get; set; }
+        public List<ComboDTO> TipVenta { get; set; }
+        public List<ComboDTO> TipoEmpleado { get; set; }
+        public List<ComboDTO> Periodos { get; set; }
+        public List<ComboDTO> Garantias { get; set; }
+    }
+}
