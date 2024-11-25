cadae20923f4febe13aa081901c3b37dd85a7058
jperalta <jperalta@JPERALTALP01>
2024-11-25 13:48:29 -0500

    se agrega estructura de alertas

---
+++ Fuentes/AHSECO.CCL.BD/AlertasBD.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using AHSECO.CCL.BE;
+using AHSECO.CCL.COMUN;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AHSECO.CCL.BE.Filtros;
+using System.Data.SqlClient;
+
+namespace AHSECO.CCL.BD
+{
+    public class AlertasBD
+    {
+        CCLog Log;
+
+        public AlertasBD() : this(new CCLog())
+        {
+        }
+
+        public AlertasBD(CCLog cclog)
+        {
+            Log = cclog;
+        }
+
+
+    }
+}
---
+++ Fuentes/AHSECO.CCL.BL/AlertasBL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AHSECO.CCL.BD;
+using AHSECO.CCL.COMUN;
+using AHSECO.CCL.BE;
+using System.DirectoryServices;
+using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Generic;
+
+namespace AHSECO.CCL.BL
+{
+    public class AlertasBL
+    {
+        private AlertasBD Repository;
+
+        private CCLog Log;
+        public AlertasBL()
+            : this(new AlertasBD(), new CCLog())
+        {
+        }
+        public AlertasBL(AlertasBD alertasBD, CCLog log)
+        {
+            Repository = alertasBD;
+            Log = log;
+        }
+
+
+
+    }
+}
