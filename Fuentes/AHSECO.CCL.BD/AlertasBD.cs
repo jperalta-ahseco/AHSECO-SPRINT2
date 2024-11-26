24087d7fb2c264fec8badfcc1c18c40c9ff17d2b
dbazalar <dbazalar@qualitygardian.com>
2024-11-26 17:08:21 -0500

    Se corrige AlertasBD y alertasBL

--- Fuentes/AHSECO.CCL.BD/AlertasBD.cs
+++ Fuentes/AHSECO.CCL.BD/AlertasBD.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AHSECO.CCL.BE.Filtros;
 using System.Data.SqlClient;
+using AHSECO.CCL.BE.ServicioTecnico.BandejaGarantias;
 
 namespace AHSECO.CCL.BD
 {
@@ -25,6 +26,34 @@ namespace AHSECO.CCL.BD
             Log = cclog;
         }
 
+        public IEnumerable<GarantiaResultDTO> ObtenerGarantiasProximasVencer()
+        {
+            using( var connection = Factory.ConnectionFactory())
+            {
+                Log.TraceInfo(Utilidades.GetCaller());
+                connection.Open();
 
+                var result = connection.Query(
+                    sql: "USP_SEL_GARANT_PROX_VENCER",
+                    param: "",
+                    commandType: CommandType.StoredProcedure)
+                    .Select(s => s as IDictionary<string, object>)
+                    .Select(i => new GarantiaResultDTO()
+                    {
+                        Id_Solicitud = i.Single(d => d.Key.Equals("SOLICITUD")).Parse<int>(),
+                        Descripcion = i.Single(d => d.Key.Equals("DESCRIPCION")).Parse<string>(),
+                        Marca = i.Single(d => d.Key.Equals("DESCMARCA")).Parse<string>(),
+                        Modelo = i.Single(d => d.Key.Equals("MODELO")).Parse<string>(),
+                        CodProducto = i.Single(d => d.Key.Equals("CODIGOPRODUCTO")).Parse<string>(),
+                        NumSerie = i.Single(d => d.Key.Equals("NUMSERIE")).Parse<string>(),
+                        FechaInstalacion = i.Single(d => d.Key.Equals("FECHAINSTALACION")).Parse<DateTime>(),
+                        ValorGarantia = i.Single(d => d.Key.Equals("VALORGARANTIA")).Parse<string>(),
+                        FechaVencimiento = i.Single(d => d.Key.Equals("FECHAVENCIMIENTO")).Parse<DateTime>()
+                    });
+
+                connection.Close();
+                return result;
+            }
+        }
     }
 }
