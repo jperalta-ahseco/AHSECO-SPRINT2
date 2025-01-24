3fc7ac8527ef1f0e163fc06fa3b7e81482e4d6a2
dbazalar <dbazalar@qualitygardian.com>
2025-01-24 08:50:10 -0500

    Se modifica plantilla de correo de finalizacion de costos

--- Scripts/Data/06_DATA_PLANTILLAS.sql
+++ Scripts/Data/06_DATA_PLANTILLAS.sql
@@ -4,9 +4,9 @@ GO
 --SELECT * FROM TBM_PLANTILLAS
 
 INSERT INTO TBM_PLANTILLAS
-VALUES('PLANCOTGER',1,'Plantilla de cotización para gerencia','pminetti@ahsecoperu.pe','','PENDIENTE EX-WORK - N° SOLICITUD {NRO_SOL}',
+VALUES('PLANCOTGER',1,'Plantilla de cotización para gerencia','pminetti@ahsecoperu.pe','','PENDIENTE COSTO FOB - N° SOLICITUD {NRO_SOL}',
 			   '<p>Sr. Pedro Minetti,</p>
-<p>Usted tiene pendiente de ingreso del EXWORK de cotizaci&oacute;n de la solicitud N&deg; {NRO_SOL} para la empresa {NOM_EMPRESA} solicitado por {NOMBRE_VENDEDOR}.</p>
+<p>Usted tiene pendiente de ingreso del costo FOB de cotizaci&oacute;n de la solicitud N&deg; {NRO_SOL} para la empresa {NOM_EMPRESA} solicitado por {NOMBRE_VENDEDOR}.</p>
 <p>Por favor ingrese al sistema para completar los datos. Ingrese al siguiente link: <a href="https://192.168.1.220/"><span data-olk-copy-source="MessageBody">https://192.168.1.220/</span></a> .</p>','SYSTEM',GETDATE(),NULL,NULL);
 
 INSERT INTO TBM_PLANTILLAS
@@ -28,10 +28,9 @@ VALUES('PLANCOTSTC',1,'Plantilla de cotización para servicio tecnico','serviciot
 <p>Usted tiene pendiente de ingreso de costos de la cotizaci&oacute;n de la solicitud N&deg; {NRO_SOL} para la empresa {NOM_EMPRESA} solicitado por {NOMBRE_VENDEDOR}.</p>
 <p>Por favor ingrese al sistema para completar los datos. Ingrese al siguiente link: <a href="https://192.168.1.220/"><span data-olk-copy-source="MessageBody">https://192.168.1.220/</span></a> .</p>','SYSTEM',GETDATE(),NULL,NULL);
 
-
 INSERT INTO TBM_PLANTILLAS
 VALUES('PLANCOTVEN',1,'Plantilla de cotización para vendedor','','','FINALIZACIÓN DE INGRESO DE COSTOS - N° SOLICITUD {NRO_SOL}',
-			   '<p>Sr(a). Vendedor(a), {NOM_VENDEDOR}</p>
+			   '<p>Estimado(a), {NOM_VENDEDOR}</p>
 <p>Se finalizó el ingreso de montos de la cotizaci&oacute;n perteneciente en la solicitud N&deg; {NRO_SOL} para la empresa {NOM_EMPRESA}.</p>
 <p>Por favor ingrese al sistema para continuar con el flujo de venta. Ingrese al siguiente link: <a href="https://192.168.1.220/"><span data-olk-copy-source="MessageBody">https://192.168.1.220/</span></a> .</p>','SYSTEM',GETDATE(),NULL,NULL);
 
