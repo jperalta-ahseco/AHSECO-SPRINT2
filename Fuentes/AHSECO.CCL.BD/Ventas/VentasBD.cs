899d39a458ad9195e84995c8e7bf8b0e71291c71
jperalta <jperalta@JPERALTALP01>
2025-02-26 14:04:04 -0500

    homologacion

--- Fuentes/AHSECO.CCL.BD/Ventas/VentasBD.cs
+++ Fuentes/AHSECO.CCL.BD/Ventas/VentasBD.cs
@@ -3462,5 +3462,14 @@ namespace AHSECO.CCL.BD.Ventas
                         GenerarGuiaPedidoSinStock = i.Single(d => d.Key.Equals("GENGP_SS")).Value.Parse<int>(),
                         GenerarGuiaBOSinStock = i.Single(d => d.Key.Equals("GENBO_SS")).Value.Parse<int>(),
                         TipoDespacho = i.Single(d => d.Key.Equals("TIPODESPACHO")).Value.Parse<string>(),
+                        GenerarGuiaManuscrita = i.Single(d => d.Key.Equals("GENMAN_SERV")).Value.Parse<int>()
+                    }).FirstOrDefault();
+
+                return result;
+            }
+        }
+
+
+
     }
 }
